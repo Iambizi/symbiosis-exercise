3f34a76ba47e5069e42771be5986ecfce0926599
Amir Bizimana <amirbzmn@gmail.com>
2019-08-16 17:14:26 -0400

    sidebar menu link hover active functionality

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
-const menu = document.getElementById("menu");
-const sidebar = document.getElementById("sidebar");
+// const menu = document.getElementById("menu");
+// const sidebar = document.getElementById("sidebar");
 
-menu.addEventListener("click", function toggle() {});
+// menu.addEventListener("click", function toggle() {});
+
+const links = document.getElementsByClassName("menu-link");
+
+for (var i = 0; i < links.length; i++) {
+  links[i].addEventListener("mouseover", function() {
+    this.className += " active";
+  });
+  links[i].addEventListener("mouseout", function() {
+    this.className -= " active";
+  });
+}
